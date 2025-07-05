d6b79cce6c2cd1b7f179c66a5b85ff260bfad8a9
Asad Atik <asadatik1995@gmail.com>
2025-07-05 13:10:05 +0600

    import cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors"; // 🔥 1. Import CORS
 
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 
-
 const app: Application = express();
+
+// 🔥 2. Use CORS middleware (allows cross-origin requests)
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", bookRoutes);
-
 app.use("/api", borrowRoutes);
 
 app.get("/", (req: Request, res: Response) => {
